a0de9bda6ad4f1e9ad5cdb7e97bb79d7058735b1
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-09-05 21:01:22 +0530

    feat: setup routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/index.tsx"),
+  route("signin", "routes/signin.tsx"),
+  route("signup", "routes/signup.tsx"),
+] satisfies RouteConfig;
